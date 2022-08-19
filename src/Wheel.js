ff6bb29da4491b8d61d07b07b9104a298b116ad5
Uvais Ahmad <uvss21@gmail.com>
2022-08-19 18:21:52 +0530

    Some small rough changes

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,22 +1,25 @@
-import React from "react";    
-const Wheel = (props)=> {
-    // constructor(){
-    //     super();
-    // }
+import React from "react"; 
 
-    return(
-        <div className="wheel">
-            <div className="outer-wheel" id="o-wheel">
-                <div className="inner-wheel">
-                <div className="play-btn menu
-                ">Menu</div>
-                <div className="play-btn forward">R</div>
-                <div className="play-btn backward">L</div>
-                <div className="play-btn bottom">B</div>
+class Wheel extends React.Component {
+    
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div className="wheel">
+                <div className="outer-wheel" id="o-wheel">
+                    <div className="inner-wheel">
+                    <div className="play-btn menu
+                    ">Menu</div>
+                    <div className="play-btn forward">R</div>
+                    <div className="play-btn backward">L</div>
+                    <div className="play-btn bottom">B</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-    
+        )
+    }
 }
 export default Wheel;
--- src/screen.js
+++
@@ -1,9 +0,0 @@
-
-const screen = () =>{
-    return(
-        <div>
-            
-        </div>
-
-    )
-}
