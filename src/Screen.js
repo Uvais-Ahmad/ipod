91ef33395b425195ce13bdc3f908d6bdc8d83021
Uvais Ahmad <uvss21@gmail.com>
2022-08-28 15:44:08 +0530

    Add and Style Artists screen

--- src/Screen.js
+++ src/Screen.js
@@ -5,6 +5,7 @@ import Music from './Music';
 import Settings from './Settings';
 import Games from './Games';
 import MyMusic from './MyMusic';
+import Artists from './Artists';
 
 class Screen extends React.Component{
     constructor(){
@@ -19,6 +20,7 @@ class Screen extends React.Component{
                 {this.props.activePage==='Music'?<Music activeItem={this.props.activeItem}/> : null}
                 {this.props.activePage==='Settings'?< Settings /> : null}
                 {this.props.activePage==='MyMusic'?<MyMusic /> : null}
+                {this.props.activePage==='Artists'?<Artists /> : null}
             </div>
         );
     }
